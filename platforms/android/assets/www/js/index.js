c3ed9fd35129405d108bcabb21db2be2361fffed
Ho Jie Feng <hjf3108@gmail.com>
2015-12-21 23:47:08 +0000

    Add proof of concept CCA page

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,3 +1,13 @@
+/*
+ * 
+ * Copyright(c) 2015 - 2016
+ * Velusamy Sathiakumar Ragul Balaji
+ * & Ho Jie Feng.
+ * Released under GNU GPL v2
+ * All rights reserved.
+ * 
+ */
+
 function start() {
-    $("#mainlogo").fadeIn(1000, function () {});
+    $("#mainlogo").fadeIn(1500, function () {});
 }
