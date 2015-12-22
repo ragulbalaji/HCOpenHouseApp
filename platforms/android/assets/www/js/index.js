054e7629b3a38ce5dd002305a9115f52113b9917
Ho Jie Feng <hjf3108@gmail.com>
2015-12-22 21:04:40 +0000

    CCA buttons generated dynamically

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -11,7 +11,26 @@
 function start() {
     $("#mainlogo").fadeIn(1500, function () {});
 
-
+	//create CCA buttons
+    for (name in ccas) {
+    	$('#' + ccas[name].type + ' ul').append(
+    		$('<li>').attr('data-filtertext',ccas[name].searchtags)
+    				 .attr('data-ccaname',name)
+    				 .append($('<a>')
+    				 		 .attr('href','#ccapage').text(ccas[name].fullname)
+    				 		 .attr('data-ccaname',name)
+    				 )
+    	);
+    		//<li data-filtertext="Infocomm Robotics Society HCIRS"><a href="#infosoc">Infocomm &amp; Robotics Society</a></li>
+    }
+    $('#cns ul li a').on('click',function(e) {
+    	var ccaname = $(this).attr('data-ccaname');
+    	$('#ccapage').css('background-image', 'url("' + ccas[ccaname].backgroundimg + '")');
+    	$('#ccamainimg').attr('src',ccas[ccaname].mainimg);
+    	$('#ccapage h1').html(ccas[ccaname].fullname);
+    	$('#ccaintro').html(ccas[ccaname].introduction.replace(/(?:\r\n|\r|\n)/g, '<br />'));
+    	$('#ccacontent').html(ccas[ccaname].content.replace(/(?:\r\n|\r|\n)/g, '<br />'));
+    });
     /*//create the cca pages
     for (name in ccas) {
         $('body').append($('<div>')
--- www/js/data.js
+++ www/js/data.js
@@ -1,17 +1,23 @@
 var ccas = {
     infosoc: {
-        fullname: "&#x1f4bb; Infocomm and Robotics Society &#x1f916;",
+        fullname: "&#x1F4BB; Infocomm and Robotics Society &#x1F916;",
         introduction: "<i>Samuel Leong, President\nVS Ragul Balaji, Vice President\n\nSessions: Wednesday & Friday (2.30pm - 5.30pm)\n\nVenue: Computer Labs / Robotics Lab\n\nHow to Join: Sign up on ISP\n</i>",
         content: "The Infocomm and Robotics Society is a place where you can create anything you wish and change the world. We are a group of individuals filled with passion to pioneer the next generation of breakthrough technologies. Here you’ll get to work with and learn from highly passionate and motivated members who are experts in their own fields -- be it algorithmic design, web stack development, Cyber Security or Electronics. You only need a open mind!\n\nIn today's world, programming & robotics are highly relevant. Under the mentorship of passionate and skilled seniors, you will get to embark on projects of your own, from collaborating on open source projects to even launching your own company. Interested in various exciting expeditions? From Robots that play in a soccer league to writing your own social network, we’ve got it all.\n\nThere are two types of competitions that we participate in: Infocomm and Robotics. For Infocomm competitions, some of those that we participate in include SPLASH Awards, the flagship app-development competition organised by the Singapore Computer Society in which we came in overall Champions last year; the National Olympiad of Informatics (NOI) organised by NUS School of Computing, where we had 2 Silver and 3 Bronze medallists last year. As for Robotics, there is the National Junior Robotics Competition (NJRC) where we came in second for Sumobot category last year; IDA Labs Tech Factor Challenge with a $4000 seed fund per team; and RoboCup Singapore.\n\nAt HCIRS we build the future. Find out more at http://infocommsociety.com/ and email us at hwachongirs@gmail.com or hit us on out facebook page \"Hwa Chong Infocomm & Robotics Society\"",
         website: "https://infocommsociety.com/",
-        backgroundimg: "/img/ccas/hcirsbg.jpg"
+        backgroundimg: "img/ccas/hcirsbg.jpg",
+        mainimg: "img/ccas/hcirs.jpg",
+        searchtags: "HCIRS Infocomm and Robotics Society Computers Cyber",
+        type: "cns"
     },
     cca_template: {
-        fullname: "CCA NAME",
+        fullname: "CCA NAME", 
         introduction: "Short info",
         content: "main body page",
         website: "https://some_website",
-        img: "../img/ccas/hcirs.jpg"
+        backgroundimg: "img/ccas/hcirs.jpg",
+        mainimg: "img/ccas/hcirs.jpg",
+        searchtags: "test test test cca",
+        type: "cns" //cns = clubs and societies, sports = Sports, perfarts = performing arts
     }
 };
 
