a626499434f52ca8e58ecf7aa3867a13468477b9
Ho Jie Feng <hjf3108@gmail.com>
2015-12-22 16:02:49 +0000

    CCA pages are now added dynamically

---
+++ platforms/ios/www/js/data.js
@@ -0,0 +1,18 @@
+var ccas = 
+
+{
+	infosoc: {
+		fullname: "Infocomm and Robotics Society",
+		introduction: "Introduction\n\nCCA Sessions: Wednesday & Friday (2.30pm - 5.30pm)\n\nVenue: Computer Labs / Robotics Lab\n\nRequirements: NIL\n\nHow to Join: Sign up on ISP\n\nParticipation in Competitions: Yes\n\n Who to contact:\nSamuel Leong, President\nsamleocw@gmail.com (8124 1038)\nVelusamy Sathia Kumar Ragul, VIce President ragulbaby@gmail.com (9724 9270)\n",
+		content: "The Infocomm and Robotics Society is a place where you can create anything you wish and change the world. We are a group of individuals filled with passion to pioneer the next generation of breakthrough technologies. Here you’ll get to work with and learn from highly passionate and motivated members who are experts in their own fields -- be it algorithmic design, web stack development, Cyber Security or Electronics. You only need a open mind!\n\nIn today's world, programming & robotics are highly relevant. Under the mentorship of passionate and skilled seniors, you will get to embark on projects of your own, from collaborating on open source projects to even launching your own company. Interested in various exciting expeditions? From Robots that play in a soccer league to writing your own social network, we’ve got it all.\n\nThere are two types of competitions that we participate in: Infocomm and Robotics. For Infocomm competitions, some of those that we participate in include SPLASH Awards, the flagship app-development competition organised by the Singapore Computer Society in which we came in overall Champions last year; the National Olympiad of Informatics (NOI) organised by NUS School of Computing, where we had 2 Silver and 3 Bronze medallists last year. As for Robotics, there is the National Junior Robotics Competition (NJRC) where we came in second for Sumobot category last year; IDA Labs Tech Factor Challenge with a $4000 seed fund per team; and RoboCup Singapore.\n\nAt HCIRS we build the future. Find out more at http://infocommsociety.com/ and email us at hwachongirs@gmail.com or hit us on out facebook page \"Hwa Chong Infocomm & Robotics Society\"",
+		website: "https://infocommsociety.com/",
+		backgroundimg: "/img/ccas/hcirsbg.jpg"
+	}
+	cca_template: {
+		fullname: "CCA NAME",
+		introduction: "Short info".
+		content: "main body page"
+		website: "https://some_website"
+		backgroundimg: "/img/ccas/ccabg.jpg"
+	}
+};
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -11,3 +11,24 @@
 function start() {
     $("#mainlogo").fadeIn(1500, function () {});
 }
+function createccapages()
+{
+	for(name in ccas)
+	{
+		alert(ccas[name]);
+		$('body').append($('<div>')
+						.attr('data-role','page')
+						.attr('id',name)
+						.attr('data-theme','a')
+						.append($('<div>')
+								.attr('data-role','content')
+								.attr('data-theme','a')
+								.append($('<center>')
+									.append($('<h2>').text(ccas[name].fullname))
+									.append($('<p>').text(ccas[name].introduction))
+									.append($('<p>').text(ccas[name].content))
+								)
+						)
+		);
+	}
+}
