0e1ca91b783333f4eeafb492a3bb098b1f41cfa8
Ragul Balaji <ragulbaby@gmail.com>
2015-12-23 20:05:48 +0800

    [Extra] Gource Test

---
+++ others/rungource.sh
@@ -0,0 +1,4 @@
+# Simple Invoker for Gource to Visualize HCOpenHouseApp
+# Ragul Balaji Dec 2015
+cd ..
+gource -s 5 --highlight-users -1280x720 --title "Hwa Chong Open House App 2016 Development Progress" --follow-user Ragul\ Balaji --output-framerate 60 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
